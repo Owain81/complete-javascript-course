fe948f87c2c0db1863dc1aca9c2ecb65cbac39d7
Owain Kleefkens <okleefkens@gmail.com>
2019-01-21 07:29:25 +0100

    start with udemy course

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,90 @@
+/***************************************
+ * 
+ * Variables and data types
+ */
+
+// var firstName = 'Owain';
+
+// console.log(firstName);
+
+// var lastName = 'Kleefkens';
+// var age = 37;
+
+// // Boolean
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+
+// job = 'developer';
+// console.log('job');
+
+// Variable naming rules
+// var _3years = 3;
+// var johnMark = 'John and Mark';
+// var if = 23;
+
+/***************************************
+ * 
+ * Variable mutation and type cohersion
+ */
+
+// var firstName = 'Owain';
+// var age = 37;
+
+// console.log(firstName + ' ' + age);
+
+// var job,
+//     isMarried;
+
+// job = 'Developer';
+// isMarried = false;
+
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+
+// // Variable mutation 
+// age = 'twenty eight';
+// job = 'driver';
+
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// var lastName = prompt('What is his lastname');
+
+//     console.log(firstName +  lastName);
+
+
+/***************************************
+ * Basic operators
+ */
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 37
+
+// Math operators
+var yearJohn = now - ageJohn;
+var yearMark = now - ageMark;
+
+console.log(yearJohn + ' ' + yearMark);
+
+console.log(now * 2);
+console.log(now + 2);
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// Typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'deze cursus is boeiend');
+
+var x;
+
+console.log(typeof x);
+
